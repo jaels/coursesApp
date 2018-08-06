555b7c474f6d42a927cb994acf8d7363f0534c22
Yael Stein <yaelst@gmail.com>
2018-08-06 13:18:34 +0200

    app functioning, needs some fine-tuning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const css = require('../styles/stylesheet.css');
-const { getTheCourses, getOneCourse, getLocation } = require('./API');
+const {getTheCourses, getOneCourse, getLocation} = require('./API');
 
 (function() {
     getTheCourses().then(courses => {
@@ -9,18 +9,30 @@ const { getTheCourses, getOneCourse, getLocation } = require('./API');
             courseDiv.setAttribute("id", key);
             courseDiv.innerHTML = courses[key].title;
             courseDiv.addEventListener("click", (e) => {
-                let courseName = e.target.id;
-                getLocation().then(res => {
-                    var geoLocation;
-                    res.country_code === "UK" ? geoLocation = "UK" : res.continent_code === "EU" ? geoLocation = "EU" : geoLocation = "NA";
-                    getOneCourse(courseName).then(courseData => {
-                        console.log(courseData);
-                        let infoDiv = document.createElement('div');
-                        infoDiv.setAttribute("class", "courseInfo");
-                        infoDiv.innerHTML = courseData.price[geoLocation].total;
-                        e.target.appendChild(infoDiv);
-                    });
-                })
+                if(e.target.childNodes.length == 1) {
+                    let courseName = e.target.id;
+                    getLocation().then(res => {
+                        var geoLocation;
+                        res.country_code === "UK"
+                            ? geoLocation = "UK"
+                            : res.continent_code === "EU"
+                                ? geoLocation = "EU"
+                                : geoLocation = "NA";
+                        getOneCourse(courseName).then(courseData => {
+                            let infoDiv = document.createElement('div');
+                            infoDiv.setAttribute("class", "courseInfo");
+                            let priceTag = document.createElement('p');
+                            infoDiv.innerHTML = `<p> Price: ${courseData.price[geoLocation].total}</p>
+                        <p> Next Start: ${courseData.start_dates[0]}</p>
+                        <p>And then: ${courseData.start_dates[1]} </p>
+                        <p>And then: ${courseData.start_dates[2]}</p>`
+                            e.target.append(infoDiv);
+                        });
+                    })
+                }
+                else {
+                    e.target.lastChild.classList.toggle("noDisplay");
+                }
             })
             document.body.appendChild(courseDiv);
         });
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
-
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: __dirname,
+    filename: './dist/bundle.js'
   },
   mode: 'development',
   module: {
